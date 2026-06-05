17c8f9f2559163aebb14a6868adba0f9a522be47
Emanuel <vpsarg711@gmail.com>
2026-06-05 19:12:25 -0300

    Primer deploy VPSARG

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function comprarSS(){
+    alert("Plan SS seleccionado");
+}
+
+function comprarGamers(){
+    alert("Plan Gamers seleccionado");
+}
+
+function comprarIphone(){
+    alert("Plan iPhone seleccionado");
+}
+
